c282baf3ec3d0a80b339e0a0372e4467e567d2db
Safi <nassarsafi@gmail.com>
2020-04-14 18:56:26 +0300

    ➕ Add example components react.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './components/Person.js'
 
-function App() {
-  return (
+class App extends Component { 
+  state = {
+    persons: [
+      { string: 'X', number: 1 },
+      { string: 'Y', number: 2 },
+      { string: 'Z', number: 3 },
+    ]
+  }
+
+  shuffleHandler = (newString) => {
+    newString = newString || 'W'
+    this.setState({ persons: [
+        { string: 'X', number: 1 },
+        { string: newString, number: 2 },
+        { string: 'Z', number: 3 },
+      ] 
+    })
+  }
+
+  changeHandler = (event) => {
+    this.setState({ persons: [
+        { string: 'X', number: 1 },
+        { string: event.target.value, number: 2 },
+        { string: 'Z', number: 3 },
+      ] 
+    })
+  }
+
+  render() { return (
     <div className="App">
-      <h1> Title  !</h1>
+      <h1>text</h1>
+      <h1>Another text</h1>
+      <button onClick={() => this.shuffleHandler('Q')}>button</button>
+      <Person handlerShuffle={this.shuffleHandler.bind(this, 'P')} string={this.state.persons[0].string} number={this.state.persons[0].number} />
+      <Person handlerChange={this.changeHandler} string={this.state.persons[1].string} number={this.state.persons[1].number} />
+      <Person string={this.state.persons[2].string} number={this.state.persons[2].number} />
     </div>
-  );
+  )}
 }
 
 export default App;
